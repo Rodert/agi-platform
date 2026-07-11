039a91ee4560f06ad3f05b38e9f83fcaeee270a9
javapub <iswangshiyu@foxmail.com>
2026-07-11 21:06:28 +0800

    del code

--- database/mysql/003_fix_video_reference_json.sql
+++
@@ -1,7 +0,0 @@
-USE `agi_platform`;
-
-UPDATE `video_tasks`
-SET
-  `images` = COALESCE(`images`, JSON_ARRAY()),
-  `videos` = COALESCE(`videos`, JSON_ARRAY()),
-  `audios` = COALESCE(`audios`, JSON_ARRAY());
